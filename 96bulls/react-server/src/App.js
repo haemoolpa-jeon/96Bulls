f8e2a81e7cbb565b35936da5ae71693afe34d7f8
Max Quirk <max.q.807@gmail.com>
2020-09-04 17:17:18 +1000

    path change

--- 96bulls/react-server/src/App.js
+++ 96bulls/react-server/src/App.js
@@ -8,7 +8,7 @@ import CurrentClass from './pages/CurrentClass.js';
 import Question1 from './pages/Question1.js';
 import Question2 from './pages/Question2.js';
 import Page5 from './pages/Page5.js';
-import Page6 from './pages/Page6.js';
+import PurchaseShapes from './pages/PurchaseShapes.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Page8 from './pages/Page8.js';
 
@@ -26,7 +26,7 @@ class App extends React.Component {
           <Route exact path="/question1" render={() => <Question1 />} />
           <Route exact path="/question2" render={() => <Question2 />} />
           <Route exact path="/page5" render={() => <Page5 />} />
-          <Route exact path="/page6" render={() => <Page6 />} />
+          <Route exact path="/purchaseshapes" render={() => <PurchaseShapes />} />
           <Route exact path="/leaderboard" render={() => <Leaderboard />} />
           <Route exact path="/page8" render={() => <Page8 />} />
         </Switch>
