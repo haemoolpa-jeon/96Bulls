e8df28e6de5cc79bd0b7585a363605058c5364c1
haemoolpa-jeon <42135681+haemoolpa-jeon@users.noreply.github.com>
2020-09-30 17:05:07 +0900

    example code of firebase CRUD
    example code for firebase CRUD in app.js

--- 96bulls/react-server/src/App.js
+++ 96bulls/react-server/src/App.js
@@ -4,6 +4,9 @@ import './style/page8.css'
 import './style/leaderboard.css'
 import './style/page5.css'
 
+//firebase
+import {firestore} from firebase;
+
 import FrontPage from './pages/FrontPage.js';
 import CurrentClass from './pages/CurrentClass.js';
 import Profile from './pages/Profile.js';
@@ -35,5 +38,61 @@ class App extends React.Component {
     );
   }
 }
+
+// Firestore CREATE
+
+const onClickHandler = (e) => {
+  e.preventDefault();
+
+  // Only when 'task' is not empty
+  if (task !== "") {
+    firestore
+      .collection("tasks")
+      .add({ todo: task }) // insert task in todo
+      .then((res) => { // 
+      
+        setTasks((prevTasks) => tasks.concat({ todo: task, id: res.id }));
+      });
+      
+    setTask("");
+  }
+};
+
+// Firestore READ
+
+const fetchData = useCallback(() => {
+  // array to store data
+  let saveData = [];
+
+  firestore
+      .collection("tasks") //  "info" collection
+      .get() // 
+      .then((docs) => {
+      docs.forEach((doc) => {
+          saveData.push({ tasks: doc.data().tasks, id: doc.id });
+      });
+      // add data to task
+      setTasks((prevTasks) => prevTasks.concat(saveData));
+  });
+}, []);
+
+useEffect(() => {
+  fetchData();
+}, [fetchData]);
+
+// Firestore DELETE
+
+const removeHandler = (id) => {
+  firestore
+    .collection("tasks")
+    .doc(id)
+    .delete()
+    .then(() => 
+      setTasks((prevTasks) =>
+        prevTasks.filter((prevTask) => id !== prevTask.id)
+      )
+    );
+};
+
 
 export default App;
