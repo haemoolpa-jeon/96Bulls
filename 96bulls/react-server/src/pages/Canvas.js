64d5fa8e8d92411fad7db116719e81b70deec784
Jesse Klein <jesse10klein@gmail.com>
2020-10-13 17:58:38 +1000

    Added rotation and size changing to canvas

--- 96bulls/react-server/src/pages/Canvas.js
+++ 96bulls/react-server/src/pages/Canvas.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import '../App.css';
 import '../style/shapes.css';
 
@@ -7,7 +7,41 @@ const EditProfile = () => {
     
   const [selected, changeSelected] = useState(false);
   const [shapes, updateShapes] = useState([]);
+  const [shapeSize, setSize] = useState(50);
+  const [rotation, setRotation] = useState(0);
   const canvas = useRef();
+
+  useEffect(() => {
+    window.addEventListener('keydown', (event) => {
+      const keyPressed = event.key;
+      if (keyPressed === 'a' || keyPressed === 'd') {
+        handleRotate(keyPressed);
+      } else if (keyPressed === 'w' || keyPressed === 's') {
+        handleResize(keyPressed);
+      }
+    });
+  }, []);
+
+  const handleResize = (key) => {
+    if (key === 'w') {
+      console.log("Bigger size");
+      setSize((prevSize) => (prevSize + 10) > 100 ? 100 : (prevSize + 5));
+    } else if (key === 's') {
+      console.log("Smaller size");
+      setSize((prevSize) => (prevSize - 10) < 10 ? 10 : (prevSize - 5));
+    }
+  }
+
+  const handleRotate = (key) => {
+    if (key === 'a') {
+      console.log("Rotating anti-clockwise");
+      setRotation((prevRotation) => ((prevRotation - 90) < 0 ? 270 : (prevRotation - 90)));
+    } else if (key === 'd') {
+      console.log("Rotating clockwise");
+      setRotation((prevRotation) => ((prevRotation + 90) >= 360 ? 0 : (prevRotation + 90)));
+    }
+  }
+
 
   const selectShape = (shapeName) => {
     console.log('selecting:', shapeName)
@@ -19,8 +53,6 @@ const EditProfile = () => {
     document.getElementById(shapeName).classList.add('selected');
     changeSelected(shapeName);
   }
-
-
 
   const selectCircle = () => {selectShape('circle')}
 
@@ -35,10 +67,18 @@ const EditProfile = () => {
     renderCanvas(evt.clientX - rect.left, evt.clientY - rect.top);
   }
 
-  const drawRectangle = (x, y, size) => {
+  const drawRectangle = (x, y, size, rotation) => {
     var ctx = canvas.current.getContext("2d");
     ctx.beginPath();
-    ctx.rect(x, y, size, size * 2);
+    if (rotation === 0) {
+      ctx.rect(x, y, size, size * 2);
+    } else if (rotation === 90) {
+      ctx.rect(x, y, -size * 2, size);
+    } else if (rotation === 180) {
+      ctx.rect(x, y, size, -size * 2);
+    } else {
+      ctx.rect(x, y, size * 2, -size);
+    }
     ctx.stroke();
   }
 
@@ -49,12 +89,23 @@ const EditProfile = () => {
     ctx.stroke();
   }
 
-  const drawTriangle = (x, y, size) => {
+  const drawTriangle = (x, y, size, rotation) => {
     var ctx = canvas.current.getContext("2d");
     ctx.beginPath();
     ctx.moveTo(x, y);
-    ctx.lineTo(x + size, y + size);
-    ctx.lineTo(x + size, y - size);
+    if (rotation === 0) {
+      ctx.lineTo(x + size, y + size);
+      ctx.lineTo(x + size, y - size);
+    } else if (rotation === 90) {
+      ctx.lineTo(x + size, y - size);
+      ctx.lineTo(x + 2 * size, y);
+    } else if (rotation === 180) {
+      ctx.lineTo(x - size, y + size);
+      ctx.lineTo(x - size, y - size);
+    } else {
+      ctx.lineTo(x + size, y + size);
+      ctx.lineTo(x + 2 * size, y);
+    }
     ctx.closePath();
     ctx.stroke();
   }
@@ -79,13 +130,13 @@ const EditProfile = () => {
     for (let i = 0; i < shapes.length; i++) {
       const shape = shapes[i];
       if (shape.type === 'circle') {
-        drawCircle(shape.x, shape.y, 20);
+        drawCircle(shape.x, shape.y, shape.size);
       } else if (shape.type === 'rectangle') {
-        drawRectangle(shape.x, shape.y, 20);
+        drawRectangle(shape.x, shape.y, shape.size, shape.rotation);
       } else if (shape.type === 'square') {
-        drawSquare(shape.x, shape.y, 20);
+        drawSquare(shape.x, shape.y, shape.size);
       } else if (shape.type === 'triangle') {
-        drawTriangle(shape.x, shape.y, 20);
+        drawTriangle(shape.x, shape.y, shape.size, shape.rotation);
       } 
     }
   }
@@ -95,13 +146,13 @@ const EditProfile = () => {
     renderSavedShapes(x, y);
 
     if (selected === 'circle') {
-      drawCircle(x, y, 20);
+      drawCircle(x, y, shapeSize);
     } else if (selected === 'rectangle') {
-      drawRectangle(x, y, 20);
+      drawRectangle(x, y, shapeSize, rotation);
     } else if (selected === 'square') {
-      drawSquare(x, y, 20);
+      drawSquare(x, y, shapeSize);
     } else if (selected === 'triangle') {
-      drawTriangle(x, y, 20);
+      drawTriangle(x, y, shapeSize, rotation);
     } else {
       return;
     }
@@ -115,7 +166,7 @@ const EditProfile = () => {
     const x = evt.clientX - rect.left;
     const y = evt.clientY - rect.top;
 
-    updateShapes([...shapes, {type: selected, x, y}]);
+    updateShapes([...shapes, {type: selected, x, y, size: shapeSize, rotation}]);
   }
 
   const handleCanvasLeave = (evt) => {
