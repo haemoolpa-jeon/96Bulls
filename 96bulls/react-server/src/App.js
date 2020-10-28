7e2a3f803ddc124662684f1de15a32904bc963fc
haemoolpa-jeon <42135681+haemoolpa-jeon@users.noreply.github.com>
2020-10-28 23:51:33 +0900

    changed app.js and index.js
    fixed store error

--- 96bulls/react-server/src/App.js
+++ 96bulls/react-server/src/App.js
@@ -1,103 +1,61 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import firebase from './config/firebase';
-import * as serviceWorker from './serviceWorker';
+import styles from './style/Home.module.css';
 
 import './App.css';
-import './style/Home.module.css';
-import 'bootstrap/dist/css/bootstrap.css';
 
-import FrontPage from './pages/FrontPage.js';
-import CurrentClass from './pages/CurrentClass.js';
-import Profile from './pages/Profile.js';
-import Leaderboard from './pages/Leaderboard.js';
-import Achievements from './pages/Achievements.js';
-import QuizPage from './pages/QuizPageAlt.js';
-import HomePage from './pages/HomePage.js';
-import CreateQuiz from './pages/CreateQuizAlt.js';
-import Quizzes from './pages/Quizzes.js';
-import ChooseUserTypePage from './pages/ChooseUserTypePage.js';
-import ChatPage from './pages/chat/components/chatPage.js'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import userTypeStore from './userTypeStore';
 
-import Login from './pages/chat/components/Auth/Login';
-import Register from './pages/chat/components/Auth/Register';
-import Spinner from './pages/chat/components/common/Spinner';
-
-import { Route, BrowserRouter as Router, Switch, withRouter } from 'react-router-dom'
-import EditProfile from './pages/Canvas.js';
-
-import { createStore } from 'redux';
-import { Provider, connect } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './pages/chat/reducers';
-import { setUser, clearUser } from './pages/chat/actions';
-
-const store = createStore(rootReducer, composeWithDevTools());
-
-class App extends Component {
-  componentDidMount() {
-    const { setUser, clearUser, history } = this.props;
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user);
-        history.push('/');
-      } else {
-        history.push('/signin');
-        clearUser();
-      }
-    });
-  }
-  
+export class App extends Component {
   render() {
-    const { isLoading } = this.props;
-    return isLoading ? (
-      <Spinner />
-    ) : (
-      <Router>
-        <Switch>
-        <Route exact path="/" component={ChooseUserTypePage} />
-        <Route exact path="/home" render={() => <HomePage />} />
-        <Route exact path="/register" render={() => <Register />} />
-        <Route exact path="/signin" render={() => <Login />} />
-        <Route exact path="/class" render={() => <FrontPage />} />
-        <Route exact path="/currentClass" render={() => <CurrentClass />} />
-        <Route exact path="/profile" render={() => <Profile name="Christopher Walken" degree="Bachelor of Copy Paste" />} />
-        <Route exact path="/editprofile" render={() => <EditProfile />} />
-        <Route exact path="/leaderboard" render={() => <Leaderboard />} />
-        <Route exact path="/achievements" render={() => <Achievements />} />
-        <Route exact path="/Quiz/:id" render={({ match }) => <QuizPage match={match} />} />
-        <Route exact path="/CreateQuiz" render={() => <CreateQuiz />} />
-        <Route exact path="/quizzes" render={() => <Quizzes />} />
-        <Route exact path="/chat" component={ChatPage} />
-      </Switch>
-    </Router>
-      
+    return (
+      <div className={styles.App}>
+        <div className={styles.container}>
+          <title>Altis Academia</title>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Welcome to UQ remote
+          </h1>
+  
+          <p className={styles.description}>
+            Powered By Altis Academia
+          </p>
+
+          <div className={styles.chooseUserPage}>
+            <div className={styles.chooseUserCard}>
+              <div className='flex-center' style={{ backgroundColor: '#BF40FF', width: '100%', height: 60, color: 'white' }}>
+                Choose user type
+              </div>
+              <div className="button" style={{ width: '200px', marginTop: 20 }} onClick={() => { userTypeStore.changeUserType('instructor');}}>
+                <Link to = "/home">Instructor</Link>
+              </div>
+              <div className="button" style={{ width: '200px' }} onClick={() => { userTypeStore.changeUserType('student'); }}>
+                <Link to = "/home">Student</Link>
+              </div>
+            </div>
+          </div>          
+          
+        </main>
+  
+        <footer className={styles.footer}>
+          <h2>Powered By Altis Academia</h2>
+        </footer>
+      </div>
+      </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  isLoading: state.user.isLoading,
+  currentUser: state.user.currentUser,
+  currentRoom: state.room.currentRoom,
 });
 
-const RootWithAuth = withRouter(connect(mapStateToProps, { setUser, clearUser })(App));
-
 App.propTypes = {
-  history: PropTypes.object.isRequired,
-  isLoading: PropTypes.bool.isRequired,
-  setUser: PropTypes.func.isRequired,
-  clearUser: PropTypes.func.isRequired,
+  currentUser: PropTypes.object,
+  currentRoom: PropTypes.object,
 };
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <RootWithAuth />
-    </Router>
-  </Provider>, document.getElementById('root'),
-);
-
-serviceWorker.unregister();
-
-export default App;
+export default connect(mapStateToProps)(App);
--- 96bulls/react-server/src/index.js
+++ 96bulls/react-server/src/index.js
@@ -1,17 +1,93 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Route, BrowserRouter as Router, Switch, withRouter } from 'react-router-dom'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import PropTypes from 'prop-types';
+import firebase from './config/firebase';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './pages/chat/reducers';
+import { setUser, clearUser } from './pages/chat/actions';
+
+import './index.css';
+
+//pages
+import FrontPage from './pages/FrontPage.js';
+import CurrentClass from './pages/CurrentClass.js';
+import Profile from './pages/Profile.js';
+import Leaderboard from './pages/Leaderboard.js';
+import QuizPage from './pages/QuizPageAlt.js';
+import HomePage from './pages/HomePage.js';
+import CreateQuiz from './pages/CreateQuizAlt.js';
+import Quizzes from './pages/Quizzes.js';
+import ChatPage from './pages/chat/components/chatPage.js'
+import Login from './pages/chat/components/Auth/Login';
+import Register from './pages/chat/components/Auth/Register';
+import Spinner from './pages/chat/components/common/Spinner';
+import EditProfile from './pages/Canvas.js';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+class Root extends Component {
+  componentDidMount() {
+    const { setUser, clearUser, history } = this.props;
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+        history.push('/');
+      } else {
+        history.push('/signin');
+        clearUser();
+      }
+    });
+  }
+
+  render() {
+    const { isLoading } = this.props;
+    return isLoading ? (
+      <Spinner />
+    ) : (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/home" render={() => <HomePage />} />
+        <Route exact path="/register" render={() => <Register />} />
+        <Route exact path="/signin" render={() => <Login />} />
+        <Route exact path="/class" render={() => <FrontPage />} />
+        <Route exact path="/currentClass" render={() => <CurrentClass />} />
+        <Route exact path="/profile" render={() => <Profile name="Christopher Walken" degree="Bachelor of Copy Paste" />} />
+        <Route exact path="/editprofile" render={() => <EditProfile />} />
+        <Route exact path="/leaderboard" render={() => <Leaderboard />} />
+        <Route exact path="/Quiz/:id" render={({ match }) => <QuizPage match={match} />} />
+        <Route exact path="/CreateQuiz" render={() => <CreateQuiz />} />
+        <Route exact path="/quizzes" render={() => <Quizzes />} />
+        <Route exact path="/chat" component={ChatPage} />
+      </Switch>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isLoading: state.user.isLoading,
+});
+
+const RootWithAuth = withRouter(connect(mapStateToProps, { setUser, clearUser })(Root));
+
+Root.propTypes = {
+  history: PropTypes.object.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  setUser: PropTypes.func.isRequired,
+  clearUser: PropTypes.func.isRequired,
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>, document.getElementById('root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
